cc56347554093b4488db995813a7815bd57c565d
Nathan <na.than.fjv@gmail.com>
2023-10-24 20:06:14 -0300

    feat: add mouses product list on home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -22,6 +22,14 @@ export default async function Home() {
       }
     }
   })
+  
+  const mouses = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "mouses"
+      }
+    }
+  })
 
   return (
     <div className="flex flex-col gap-10">
@@ -47,6 +55,11 @@ export default async function Home() {
 
       <div>
         <Banner src="/banner-home-03.png" alt="Até 20% de desconto em fones" />
+      </div>
+
+      <div>
+        <SectionTitle title="Mouses" />
+        <ProductList products={mouses} />
       </div>
     </div>
   )
